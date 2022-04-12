e830e4300bcc8bdde7c0443b28ba04900a2ec6c7
Sophoan Meas <sophoan.meas@hotmail.com>
2022-04-11 23:09:12 -0400

    questions

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-const generatePage = require('./src/page-template.js');
+const inquirer = require("inquirer")
 
-const fs = require('fs');
+inquirer
+    .prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'what is your name?'
+    }])
+    .then(answer => console.log(answer));
 
-const profileDataArgs = process.argv.slice(2);
 
-const [name, github] = profileDataArgs;
+// const generatePage = require('./src/page-template.js');
+// const fs = require('fs');
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+// const profileDataArgs = process.argv.slice(2);
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// const [name, github] = profileDataArgs;
+
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw new Error(err);
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
